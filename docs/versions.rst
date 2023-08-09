569d901b948c28f59858204262492c6312e0f967
David Fulford <dfulford@eivcapital.com>
2023-08-09 16:25:34 -0500

    update version to 1.1.0
    Signed-off-by: David Fulford <dfulford@eivcapital.com>

--- docs/versions.rst
+++ docs/versions.rst
@@ -6,7 +6,7 @@ Version History
    :noindex:
 
 
-1.0.9
+1.1.0
 -----
 
 * Bug Fix
--- petbox/dca/__init__.py
+++ petbox/dca/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.9'
+__version__ = '1.1.0'
 
 from .base import (get_time, get_time_monthly_vol,
                    DeclineCurve, PrimaryPhase,
